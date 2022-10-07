883844d7fb88eff2518f4efcf3141c6f80e6ba7d
Prashant Mishra <prashant.mishra@salesforce.com>
2022-10-07 21:15:18 +0530

    formating

--- force-app/main/default/lwc/textBoxWithChackBoxJest/checkBoxJest/__tests__/checkBoxJest.test.js
+++ force-app/main/default/lwc/textBoxWithChackBoxJest/checkBoxJest/__tests__/checkBoxJest.test.js
@@ -1,42 +1,40 @@
-import { createElement } from 'lwc';
-import CheckBoxJest from 'c/checkBoxJest';
+import { createElement } from "lwc";
+import CheckBoxJest from "c/checkBoxJest";
 
-describe('c-check-box-jest', () => {
-    beforeEach(() => {
-        const element = createElement('c-check-box-jest', {
-            is: CheckBoxJest
-        })
-        document.body.appendChild(element);
-        // The jsdom instance is shared across test cases in a single file so reset the DOM
+describe("c-check-box-jest", () => {
+  beforeEach(() => {
+    const element = createElement("c-check-box-jest", {
+      is: CheckBoxJest
     });
+    document.body.appendChild(element);
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+  });
 
-    it('Password checkbox Test With Flase', () => {
-        const element = document.querySelector('c-check-box-jest');
-        const passwordElement = element.shadowRoot.querySelector('.userInfo');
-        expect(passwordElement.textContent).toBe('My Password is ********');
+  it("Password checkbox Test With Flase", () => {
+    const element = document.querySelector("c-check-box-jest");
+    const passwordElement = element.shadowRoot.querySelector(".userInfo");
+    expect(passwordElement.textContent).toBe("My Password is ********");
+  });
 
+  it("Password checkbox Test With True", () => {
+    const element = document.querySelector("c-check-box-jest");
+
+    // Set value in text Box
+    const textBoxValue = element.shadowRoot.querySelector(".textBox");
+    textBoxValue.value = "test";
+    textBoxValue.dispatchEvent(new CustomEvent("change"));
+
+    // Set true value in Checkbox
+    const checkBoxValue = element.shadowRoot.querySelector(".checkBox");
+    checkBoxValue.value = true;
+    checkBoxValue.dispatchEvent(new CustomEvent("change"));
+
+    // Getting actual Result
+    const passwordElement = element.shadowRoot.querySelector(".userInfo");
+
+    // assert using Promise
+    return Promise.resolve().then(() => {
+      expect(passwordElement.textContent).toBe("My Password is test");
     });
-
-    it('Password checkbox Test With True', () => {
-        const element = document.querySelector('c-check-box-jest');
-        
-        // Set value in text Box
-        const textBoxValue = element.shadowRoot.querySelector(".textBox");
-        textBoxValue.value="test";
-        textBoxValue.dispatchEvent(new CustomEvent('change'));
-        
-        // Set true value in Checkbox
-        const checkBoxValue = element.shadowRoot.querySelector(".checkBox");
-        checkBoxValue.value=true;
-        checkBoxValue.dispatchEvent(new CustomEvent('change'));
-
-        // Getting actual Result
-        const passwordElement = element.shadowRoot.querySelector('.userInfo');
-
-        // assert using Promise
-        return Promise.resolve().then(()=>{
-            expect(passwordElement.textContent).toBe('My Password is test');
-        })
-
-    });
+  });
 });
